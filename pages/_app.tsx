ad9640be64f6533e1148655052f63d2436004258
Fernando Vera <fernandoveragonzalez@gmail.com>
2022-03-07 17:43:13 -0300

    pre-deployment

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import { AppProps } from 'next/app'
+import { NextPage } from 'next/types'
+import { ReactElement, ReactNode } from 'react'
+import '../styles/globals.css'
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+
+  const getLayout = Component.getLayout || ((page) => page)
+
+  //return <Component {...pageProps} />
+  return getLayout(<Component {...pageProps} />)
+}
+
+export default MyApp
